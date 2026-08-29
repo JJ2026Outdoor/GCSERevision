879dfea9ad3016f2e68dcd3183eb95f58be08f89
JJ2026Outdoor <jroj2000@gmail.com>
2026-08-29 20:05:03 +0100

    Add files via upload

--- firebase-config.js
+++ firebase-config.js
@@ -5,12 +5,13 @@
 // daughter's phone. Nothing else in the app needs to change when you add
 // your real values here.
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyB2tAEcmzKPBgD7UOKqXibvP2f3O96ASoM",
+  authDomain: "gcserevisionjj2026.firebaseapp.com",
+  projectId: "gcserevisionjj2026",
+  storageBucket: "gcserevisionjj2026.firebasestorage.app",
+  messagingSenderId: "858477367129",
+  appId: "1:858477367129:web:53f242e1dc156f3c130648",
+  measurementId: "G-B42LE1PND9"
 };
 
 export const isFirebaseConfigured = firebaseConfig.apiKey !== "YOUR_API_KEY" && !!firebaseConfig.apiKey;
